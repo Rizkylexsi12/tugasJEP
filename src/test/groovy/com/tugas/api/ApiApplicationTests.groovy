472f0ce4001d2bed916162a505d542bb21e1a614
Rizkylexsi12 <rizkylexsi@gmail.com>
2023-10-13 23:13:42 +0700

    first commit

---
+++ src/test/groovy/com/tugas/api/ApiApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.tugas.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
